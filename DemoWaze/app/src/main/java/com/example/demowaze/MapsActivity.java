32acfff452324f18eae26a529670a3b2e628a33d
BenTheTurtle <53292800+bentheturtle@users.noreply.github.com>
2021-11-29 17:15:42 +0200

    cleaned some useless documentation

--- DemoWaze/app/src/main/java/com/example/demowaze/MapsActivity.java
+++ DemoWaze/app/src/main/java/com/example/demowaze/MapsActivity.java
@@ -31,23 +31,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     *
-     * this is a template.
-     */
+    //sets the map to the set marker (in akko)
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
+        // Add a marker in akko and move the camera
         double latitude = 32.92526122932048;
         double longitude = 35.07909462532516;
         LatLng akko = new LatLng(latitude, longitude);
